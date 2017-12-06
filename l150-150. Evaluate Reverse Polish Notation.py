923028bab40aaa2fcce08a4b8f2a7c62be52d1bd
minging234 <825876887@qq.com>
2017-12-06 11:33:30 -0500

    12/6

---
+++ l150-150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        numbers = []
+        for i in tokens:
+            if i == '+':
+                n2 = numbers.pop()
+                n1 = numbers.pop()
+                numbers.append(n1 + n2)
+            elif i == '-':
+                n2 = numbers.pop()
+                n1 = numbers.pop()
+                numbers.append(n1 - n2)
+            elif i == '*':
+                n2 = numbers.pop()
+                n1 = numbers.pop()
+                numbers.append(n1 * n2)
+            elif i == '/':
+                n2 = numbers.pop()
+                n1 = numbers.pop()
+                if n1*n2 <0 and n1%n2 !=0:
+                    numbers.append(n1/n2+1)
+                else:
+                    numbers.append(n1 / n2)
+            else:
+                numbers.append(int(i))
+        return numbers[0]
+
