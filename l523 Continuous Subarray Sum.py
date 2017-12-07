64298ccc28009feaf9a1a6c0f8302faf0f9b8db0
minging234 <825876887@qq.com>
2017-12-06 20:35:57 -0500

    finish 523'

---
+++ l523 Continuous Subarray Sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+
+        i = 0
+        while i < len(nums):
+            j = i
+            ssum = 0
+            while j < len(nums)-i:
+                ssum = ssum + j
+                if k == 0 and ssum == 0 and j > i:
+                    return True
+                elif k != 0 and ssum % k == 0 and j > i:
+                    return True
+                j += 1
+            i += 1
+        return False
+
+
+
+
