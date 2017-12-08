a740221084202f89b16c276cbf8dcd66f4e8d586
minging234 <825876887@qq.com>
2017-12-08 10:21:31 -0500

    move to vscode

--- l460 LFU Cache.py
+++ l460 LFU Cache.py
@@ -5,19 +5,27 @@ class LFUCache(object):
         """
         self.lim = capacity
         self.n = 0
-        self.log = 0
+        self.order = []
         self.cdict = {}
-         # "name": value, count, log
+        print('init')
+        # "name": value, count
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        self.log += 1
         if key in self.cdict:
             self.cdict[key][1] += 1
-            self.cdict[key][2] = self.log
+            pos = self.order.index(key)
+            adr = pos
+            val = self.cdict[key][1]
+            while self.cdict[self.order[pos]][1] <= val:
+                self.order[adr], self.order[pos] = self.order[pos], self.order[adr]
+                adr = pos
+                pos -= 1
+                if pos < 0:
+                    break
             return self.cdict[key][0]
         else:
             return -1
@@ -28,10 +36,54 @@ class LFUCache(object):
         :type value: int
         :rtype: void
         """
-        self.log += 1
         if key in self.cdict:
             self.cdict[key][0] = value
             self.cdict[key][1] += 1
-            self.cdict[key][2] = self.log
-        elif self.n > self.lim:
+            pos = self.order.index(key)
+            adr = pos
+            val = self.cdict[key][1]
+            while self.cdict[self.order[pos]][1] <= val:
+                self.order[adr], self.order[pos] = self.order[pos], self.order[adr]
+                adr = pos
+                pos -= 1
+                if pos < 0:
+                    break
+        elif self.n >= self.lim > 0:
+            obk = self.order.pop()
+            del self.cdict[obk]
+            self.order.append(key)
+            self.cdict[key] = [value, 0]
+            pos = self.order.index(key)
+            adr = pos
+            val = self.cdict[key][1]
+            while self.cdict[self.order[pos]][1] <= val:
+                self.order[adr], self.order[pos] = self.order[pos], self.order[adr]
+                adr = pos
+                pos -= 1
+                if pos < 0:
+                    break
+        elif self.n < self.lim:
+            self.order.append(key)
+            self.cdict[key] = [value, 0]
+            pos = self.order.index(key)
+            adr = pos
+            val = self.cdict[key][1]
+            while self.cdict[self.order[pos]][1] <= val:
+                self.order[adr], self.order[pos] = self.order[pos], self.order[adr]
+                adr = pos
+                pos -= 1
+                if pos < 0:
+                    break
+            self.n += 1
+            print(self.order)
 
+
+a = LFUCache(3)
+print(
+a.put(1,1),
+a.put(2,2),
+a.put(3,3),
+a.put(4,4),
+a.get(4),a.get(3),a.get(2),a.get(1)
+
+)
--- text.py
+++ text.py
@@ -1,3 +1,11 @@
-dci = {}
-dci["a"]=[1,2,3]
-print(dci["a"])
+# dci = {}
+# dci["a"]=[1,2,3]
+# print(dci["a"])
+
+a = [1,2,3,3,4]
+# a[0],a[1] = a[1], a[0]
+print(a)
+
+pos = a.index(4)
+while a[pos] <= val:
+    pos -= 1
